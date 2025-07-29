105636918ed65804913f489695e875a39aee997c
Malihafatima1 <malihafatima6360@gmail.com>
2025-07-29 09:40:32 -0400

    Update ProductList layout and styling

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,47 +274,49 @@ function ProductList({ onHomeClick }) {
     };
      return (
         <div>
-            <div className="navbar" style={styleObj}>
-                <div className="tag">
-                    <div className="luxury">
-                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/" onClick={handleHomeClick}>
-                            <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
-                            </div>
-                        </a>
-                    </div>
+            <div className="navbar">
+    <div className="tag">
+        <div className="luxury">
+            <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+            <a href="/" onClick={handleHomeClick} className="tag_home_link">
+                <div>
+                    <h3>Paradise Nursery</h3>
+                    <i>Where Green Meets Serenity</i>
                 </div>
-                <div style={styleObjUl}>
-                    <div><a href="#" onClick={handlePlantsClick} style={styleA}>Plants</a></div>
-                    <div>
-                        <a href="#" onClick={handleCartClick} style={styleA}>
-                            <h1 className='cart' style={{ position: 'relative' }}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
-                                    <circle cx="80" cy="216" r="12"></circle>
-                                    <circle cx="184" cy="216" r="12"></circle>
-                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
-                                </svg>
-                                {totalItems > 0 && (
-                                    <span style={{
-                                        position: 'absolute',
-                                        top: '50%',
-                                        left: '50%',
-                                        transform: 'translate(-50%, -50%)',
-                                        color: 'white',
-                                        fontSize: '18px',
-                                        fontWeight: 'bold',
-                                        pointerEvents: 'none'
-                                    }}>
-                                        {totalItems}
-                                    </span>
-                                )}
-                            </h1>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            </a>
+        </div>
+    </div>
+
+    <div className="navbar-links">
+        <div><a href="#" onClick={handlePlantsClick}>Plants</a></div>
+        <div>
+            <a href="#" onClick={handleCartClick}>
+                <h1 className="cart">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
+                        <circle cx="80" cy="216" r="12"></circle>
+                        <circle cx="184" cy="216" r="12"></circle>
+                        <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
+                            fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                    </svg>
+                    {totalItems > 0 && (
+                        <span style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            color: 'white',
+                            fontSize: '18px',
+                            fontWeight: 'bold',
+                            pointerEvents: 'none'
+                        }}>
+                            {totalItems}
+                        </span>
+                    )}
+                </h1>
+            </a>
+        </div>
+    </div>
+</div>
 
             {!showCart ? (
                 <div className="product-grid">
